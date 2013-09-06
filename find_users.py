a69a996fa5806db587a59e2b171c7380f01916bf
Bryan Fleming <bryanfleming@gmail.com>
2013-09-06 18:15:13 -0500

    its in the infant stages but right now it will grab all the users on a linux system using python

---
+++ find_users.py
@@ -0,0 +1,18 @@
+from re import findall
+from subprocess import check_output
+from pwd import getpwall
+
+d = {}
+filesearch = open("/etc/login.defs","r")
+
+for line in filesearch:
+	if findall("^UID_(MIN|MAX)",line):
+		a = line.strip().split()
+		d[str(a[0])] = str(a[1])
+
+filesearch.close()
+
+for p in getpwall():
+	if int(p[2]) >= int(d['UID_MIN']) and int(p[2]) <= int(d['UID_MAX']):
+		if str(p[0]) != "rack":
+			print p[0]

b93ea19f00741f6c5bacfd15864ad49a1c8001fd
Bryan Fleming <bryanfleming@gmail.com>
2013-09-06 17:59:13 -0500

    added a usage to make sure root is executing it. I also made sure to produce the total for / at the end (might take longer to produce count but can be informational

--- dirCount.sh
+++ dirCount.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+usage() {
+	echo "Please use process as root.. exiting"
+	exit 1
+}
+
+[[ `id -u` -ne 0 ]] && usage
+
+echo "-------";
 # Complete a search on root directory (exclude root)
-for i in $(find / -maxdepth 1 -type d | egrep -v "^/$"); 
+for i in $(find / -maxdepth 1 -type d | egrep -v "^/$" | sort); 
 do 
 	#If the directory is /proc, skip
-	[ "$i" == "/proc" ] && continue;
+	[[ "$i" == "/proc" ]] && continue;
 	#If the directory is /sys, skip
-	[ "$i" == "/sys" ] && continue; 
+	[[ "$i" == "/sys" ]] && continue;
+	#If the directory 
 	echo -ne "$i:\t\t\t"; find $i -type f | wc -l; echo "-------"; 
 done
+echo -ne "/:\t\t\t"; find / -type f | wc -l;
+echo "-------";
 
-## One Liner
-#for i in $(find / -maxdepth 1 -type d | egrep -v "^/$"); do [ "$i" == "/proc" ] && continue; [ "$i" == "/sys" ] && continue; echo -ne "$i:\t\t\t"; find $i -type f | wc -l; echo "-------"; done
+
+## One Liner (run as root) ##
+#for i in $(find / -maxdepth 1 -type d | egrep -v "^/$" | sort); do [ "$i" == "/proc" ] && continue; [ "$i" == "/sys" ] && continue; echo -ne "$i:\t\t\t"; find $i -type f | wc -l; echo "-------"; done echo -ne "/:\t\t\t"; find / -type f | wc -l; echo "-------";
